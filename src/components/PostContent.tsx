cf400d9b59f5fbef526c69e4b5deaf49b4b81e11
Christian Lopez <chrisprodev@gmail.com>
2022-05-24 13:02:04 -0500

    Update Post

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Post } from "../models/post";
+import { noZero } from "../utils/noZero";
 
 const PostContent: React.FC<Post> = ({
   img,
@@ -39,7 +40,7 @@ const PostContent: React.FC<Post> = ({
                 d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
               />
             </svg>
-            {likes}
+            {noZero(likes)}
           </span>
           <span>
             <svg
@@ -55,7 +56,7 @@ const PostContent: React.FC<Post> = ({
                 d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
               />
             </svg>
-            {comments}
+            {noZero(comments)}
           </span>
           <span>
             <svg
@@ -71,7 +72,7 @@ const PostContent: React.FC<Post> = ({
                 d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
               />
             </svg>
-            {fav}
+            {noZero(fav)}
           </span>
         </Interactions>
       </ContentWrapper>
@@ -98,8 +99,7 @@ const ContentWrapper = styled.div`
 
 const ContentHeader = styled.div`
   display: flex;
-  align-items: center;
-  margin-top: 0.6rem;
+  flex-direction: column;
 
   h4 {
     margin: 0;
@@ -107,7 +107,6 @@ const ContentHeader = styled.div`
   }
 
   small {
-    margin-left: 0.5rem;
     font-size: 1.6rem;
     color: #707070;
   }
---
+++ src/utils/noZero.ts
@@ -0,0 +1,4 @@
+export const noZero = (num: number) => {
+  if (num > 0) return num;
+  return null;
+};
